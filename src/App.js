85ef0f3e930da1d6352ddc2c3fddbe088bfd8fbe
DonizeteSantos11 <135053824+DonizeteSantos11@users.noreply.github.com>
2023-08-08 14:28:52 -0300

    1

--- src/App.js
+++ src/App.js
@@ -1,70 +1,90 @@
- import React from "react";
- import "./App.css"
+import { ToastContainer, toast } from 'react-toastify';
+import React, {useState} from "react";
 import axios from "axios";
+import "./App.css"
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
+	const [pokemons, setPokemons] = useState([]);
+	const [txtPokemon, setTxtPokemon] = useState("");
 
-	const [pokemons, alteraPokemons ] = React.useState([]);
-	const [txtpokemon, alteratxtpokemon] = React.useState("");
 
 	function buscaPokemon(){
-	
+    if(!txtPokemon.trim()){
+      buscaTodosPokemons()
+      return;
+    }
+		axios.get(`https://pokeapi.co/api/v2/pokemon/${txtPokemon}`)
+		.then(resposta => { 
+      setPokemons([resposta.data])
+		})
+		.catch(resposta=> {
+      toast("Pokémon Não Encontrado!✖");
+		})
 	}
 
-	
 	function buscaTodosPokemons(){
-	//pokemon  -> Busca todos pokemons 
-	//pokemon?limit=X -> busca todos com um numero limte
-	//pokemon/nome -> busca  um pokemon especifico	
-	 axios.get("https://pokeapi.co/api/v2/pokemon?limit=50")
-	 .then(response => {
-		console.log("Requição bem sucedida!")
-		alteraPokemons ( response.data.results)
-	 }) // será executado quando a requisição terminar 
-
-	 .catch( response => {
-		console.log("Deu ruim na requisição!")
-		console.log(response)
-	 } ) // È executado quando dá erro na requisição
-	
+		// pokemon 			-> busca todos pokemons
+		// pokemon?limit=X  -> busca todos com um numero limite
+		// pokemon/nome 	-> busca um pokemon específico
+		axios.get("https://pokeapi.co/api/v2/pokemon?limit=75")
+		.then(response => { // Será executado quando a requisição terminar
+			console.log("Requisição bem sucedida!");
+			setPokemons(response.data.results);
+		})
+		.catch(response => { // É executado quando dá erro na requisição
+			console.log("Deu ruim na requisição");
+			console.log(response);
+		}) 
 	}
-
-	// if(pokemons.length == 0){
-	// 	buscaTodosPokemons();
-	// }
-
-	React.useEffect( ()=>{
+	React.useEffect(()=>{
 		buscaTodosPokemons();
-	}, [] );
-	
-	console.log(pokemons)
-
+	}, [])
+ 
   return (
-      <div>
-		
-		<h1> Donizete PokéDex</h1>
-		<p> Conheça os Pokémons mais famosos</p>
-				
+    <div>
+      <h1>Donizete PokéDex</h1>
+		  <p>Conheça os Pokémons mais famosos</p>
+      <input onChange={(e)=>setTxtPokemon(e.target.value)} placeholder="Digite o nome de um Pokémon"/>
+      <button onClick={()=>buscaPokemon()}>Buscar</button>
 
-		<input  onChange={ (e)=> alteratxtpokemon ( e.target.value) } placeholder="Digite o nome de um Pokémon "/>
-		<button onClick={()=> buscaPokemon()}>Buscar</button>
+      <ToastContainer
+        position="top-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
 
 		{
-			pokemons.map( (pokemon, index) =>
-				<div>
+      pokemons.map((pokemon) => 
+      {
+        let id;
+        if(pokemon.url){
+          const PokemonURL = "https://pokeapi.co/api/v2/pokemon/"
+          id = pokemon.url.replace(PokemonURL, "").replace("/", "")
+        }
+        else {
+          id = pokemon.id
+        }
+        return(
+          <div className="tkmon" key={id}>
 					<p>{pokemon.name}</p>
-					<img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/"+(index+1)+".gif"}/>
-
+					<img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/"+(id)+".gif"}/>
 				</div>
+        )
+      }
 				
-				 )
+			)
 		}
-
-	</div>
-
+    </div>
   );
 }
-
 
 export default App;
